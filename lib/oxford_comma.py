035e23c21e9a61a575db226f9803b8571c8b3b59
Miriam Wahito Maina <miriammaina51@gmail.com>
2023-11-28 00:06:47 +0300

    Complete

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,21 @@
 def oxford_comma(items):
-    return None
+    if len(items) == 0:
+        return ""
+    elif len(items) == 1:
+        return items[0]
+    elif len(items) == 2:
+        list_items = []
+        list_items = ' and '.join(items)
+        return list_items
+    else:
+        # add the items to the item list except the last one 
+        joined_item = ", ".join(items[:-1])
+
+        #Add and just before the last item
+        oxford_string = f"{joined_item}, and {items[-1]}"
+
+        return oxford_string
+    
+
+
+oxford_comma(["fiddleheads", "okra", "kohlrabi"])
